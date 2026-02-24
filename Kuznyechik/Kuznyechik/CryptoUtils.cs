2e27dd865e6c1ad8523c8497dcbd8b3635ff58dd
Integer <toxaserega14@gmail.com>
2026-02-24 17:19:20 +0300

    Небольшая оптимизация линейного преобразования блока: удалёны лишние методы и циклы перенесены в блок fixed.

--- Kuznyechik/Kuznyechik/CryptoUtils.cs
+++ Kuznyechik/Kuznyechik/CryptoUtils.cs
@@ -77,7 +77,7 @@ namespace Kuznyechik
         /// </summary>
         /// <param name="block">Блок данных.</param>
         /// <param name="replaceBytes">таблица для нелинейного преобразования.</param>
-        internal static unsafe void ReplaceBytes(ref Block block, in ReadOnlySpan<byte> replaceBytes)
+        internal static void ReplaceBytes(ref Block block, in ReadOnlySpan<byte> replaceBytes)
         {
             scoped ref byte blockPtr = ref Unsafe.As<Block, byte>(ref block);
             scoped ref byte replaceBytesPtr = ref MemoryMarshal.GetReference(replaceBytes);
@@ -94,81 +94,37 @@ namespace Kuznyechik
         /// </summary>
         /// <param name="block">Блок.</param>
         /// <param name="parameters">Параметры.</param>
-        internal static void LinearTransformEncrypt(ref Block block, in CryptoParameters parameters)
-        {
-            for (int i = 0; i < BlockSize; i++)
-            {
-                TransformBlock(ref block, parameters);
-            }
-        }
-
-        /// <summary>
-        /// Трансформация блока.
-        /// </summary>
-        /// <param name="block">Блок.</param>
-        /// <param name="parameters">Параметры.</param>
-        private static unsafe void TransformBlock(ref Block block, in CryptoParameters parameters)
+        internal static unsafe void LinearTransformEncrypt(ref Block block, in CryptoParameters parameters)
         {
             scoped ref readonly GaloisTable galoisTable = ref parameters.GaloisTable;
-            byte copyLength = BlockSize - 1;
+            const byte copyLength = BlockSize - 1;
 
             fixed (byte* blockPtr = &Unsafe.As<Block, byte>(ref block))
             {
-                byte sum = galoisTable[blockPtr[0], 0];
+                for (int i = 0; i < BlockSize; i++)
+                {
+                    // Вычисляем сумму с использованием таблицы Галуа
+                    byte sum = galoisTable[blockPtr[0], 0];
+                    sum ^= galoisTable[blockPtr[1], 1];
+                    sum ^= galoisTable[blockPtr[2], 2];
+                    sum ^= galoisTable[blockPtr[3], 3];
+                    sum ^= galoisTable[blockPtr[4], 4];
+                    sum ^= galoisTable[blockPtr[5], 5];
+                    sum ^= galoisTable[blockPtr[6], 6];
+                    sum ^= galoisTable[blockPtr[7], 7];
+                    sum ^= galoisTable[blockPtr[8], 8];
+                    sum ^= galoisTable[blockPtr[9], 9];
+                    sum ^= galoisTable[blockPtr[10], 10];
+                    sum ^= galoisTable[blockPtr[11], 11];
+                    sum ^= galoisTable[blockPtr[12], 12];
+                    sum ^= galoisTable[blockPtr[13], 13];
+                    sum ^= galoisTable[blockPtr[14], 14];
+                    sum ^= galoisTable[blockPtr[15], 15];
 
-                sum ^= galoisTable[blockPtr[1], 1];
-                sum ^= galoisTable[blockPtr[2], 2];
-                sum ^= galoisTable[blockPtr[3], 3];
-                sum ^= galoisTable[blockPtr[4], 4];
-                sum ^= galoisTable[blockPtr[5], 5];
-                sum ^= galoisTable[blockPtr[6], 6];
-                sum ^= galoisTable[blockPtr[7], 7];
-                sum ^= galoisTable[blockPtr[8], 8];
-                sum ^= galoisTable[blockPtr[9], 9];
-                sum ^= galoisTable[blockPtr[10], 10];
-                sum ^= galoisTable[blockPtr[11], 11];
-                sum ^= galoisTable[blockPtr[12], 12];
-                sum ^= galoisTable[blockPtr[13], 13];
-                sum ^= galoisTable[blockPtr[14], 14];
-                sum ^= galoisTable[blockPtr[15], 15];
-
-                Buffer.MemoryCopy(blockPtr + 1, blockPtr, copyLength, copyLength);
-                blockPtr[15] = sum;
-            }
-        }
-
-        /// <summary>
-        /// Обратная трансформация блока.
-        /// </summary>
-        /// <param name="block">Блок.</param>
-        /// <param name="parameters">Параметры.</param>
-        private static unsafe void ReverseTransformBlock(ref Block block, in CryptoParameters parameters)
-        {
-            scoped ref readonly GaloisTable galoisTable = ref parameters.GaloisTable;
-            byte copyLength = BlockSize - 1;
-
-            fixed (byte* blockPtr = &Unsafe.As<Block, byte>(ref block))
-            {
-                byte sum = blockPtr[15];
-                Buffer.MemoryCopy(blockPtr, blockPtr + 1, copyLength, copyLength);
-
-                sum ^= galoisTable[blockPtr[15], 15];
-                sum ^= galoisTable[blockPtr[14], 14];
-                sum ^= galoisTable[blockPtr[13], 13];
-                sum ^= galoisTable[blockPtr[12], 12];
-                sum ^= galoisTable[blockPtr[11], 11];
-                sum ^= galoisTable[blockPtr[10], 10];
-                sum ^= galoisTable[blockPtr[9], 9];
-                sum ^= galoisTable[blockPtr[8], 8];
-                sum ^= galoisTable[blockPtr[7], 7];
-                sum ^= galoisTable[blockPtr[6], 6];
-                sum ^= galoisTable[blockPtr[5], 5];
-                sum ^= galoisTable[blockPtr[4], 4];
-                sum ^= galoisTable[blockPtr[3], 3];
-                sum ^= galoisTable[blockPtr[2], 2];
-                sum ^= galoisTable[blockPtr[1], 1];
-
-                blockPtr[0] = sum;
+                    // Сдвигаем байты и записываем новое значение в конец
+                    Buffer.MemoryCopy(blockPtr + 1, blockPtr, copyLength, copyLength);
+                    blockPtr[15] = sum;
+                }
             }
         }
 
@@ -177,11 +133,41 @@ namespace Kuznyechik
         /// </summary>
         /// <param name="block">Блок.</param>
         /// <param name="parameters">Параметры.</param>
-        private static void LinearTransformDecrypt(ref Block block, in CryptoParameters parameters)
+        private static unsafe void LinearTransformDecrypt(ref Block block, in CryptoParameters parameters)
         {
-            for (int i = 0; i < BlockSize; i++)
+            scoped ref readonly GaloisTable galoisTable = ref parameters.GaloisTable;
+            const byte copyLength = BlockSize - 1;
+
+            fixed (byte* blockPtr = &Unsafe.As<Block, byte>(ref block))
             {
-                ReverseTransformBlock(ref block, parameters);
+                for (int i = 0; i < BlockSize; i++)
+                {
+                    // Сохраняем последний байт для последующего XOR
+                    byte sum = blockPtr[15];
+
+                    // Сдвигаем байты вправо (копируем с начала в конец со смещением)
+                    Buffer.MemoryCopy(blockPtr, blockPtr + 1, copyLength, copyLength);
+
+                    // Применяем обратные преобразования с использованием таблицы Галуа
+                    sum ^= galoisTable[blockPtr[15], 15];
+                    sum ^= galoisTable[blockPtr[14], 14];
+                    sum ^= galoisTable[blockPtr[13], 13];
+                    sum ^= galoisTable[blockPtr[12], 12];
+                    sum ^= galoisTable[blockPtr[11], 11];
+                    sum ^= galoisTable[blockPtr[10], 10];
+                    sum ^= galoisTable[blockPtr[9], 9];
+                    sum ^= galoisTable[blockPtr[8], 8];
+                    sum ^= galoisTable[blockPtr[7], 7];
+                    sum ^= galoisTable[blockPtr[6], 6];
+                    sum ^= galoisTable[blockPtr[5], 5];
+                    sum ^= galoisTable[blockPtr[4], 4];
+                    sum ^= galoisTable[blockPtr[3], 3];
+                    sum ^= galoisTable[blockPtr[2], 2];
+                    sum ^= galoisTable[blockPtr[1], 1];
+
+                    // Записываем результат в первый байт
+                    blockPtr[0] = sum;
+                }
             }
         }
     }
